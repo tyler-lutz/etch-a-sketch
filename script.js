cefac1580622553e755660e13b37a7b7e8571066
Tyler Lutz <66930763+tyler-lutz@users.noreply.github.com>
2022-12-10 23:34:50 -0600

    Add mouseover event to change color of squares on grid

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const gridContainer = document.getElementById('gridContainer');
 
+function changeColor(e) {
+    console.log(e);
+    e.target.style.backgroundColor = `#121212`;
+    console.log('test')
+}
+
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -7,6 +13,7 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColor);
         gridContainer.appendChild(gridElement);
     }
 }
