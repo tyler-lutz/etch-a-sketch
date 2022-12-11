b604247262506e464e17f6e7ff6dee32834dea9d
Tyler Lutz <66930763+tyler-lutz@users.noreply.github.com>
2022-12-10 23:28:37 -0600

    Add functionality to create a grid of squares

---
+++ script.js
@@ -0,0 +1,16 @@
+const gridContainer = document.getElementById('gridContainer');
+
+function createGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridContainer.appendChild(gridElement);
+    }
+}
+
+window.onload = () => {
+    createGrid(16);
+}
